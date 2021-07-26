60d71ebf63ca0c63659df81b8cad7019ddfa3983
tomoshimizu <tomotomo.061160@gmail.com>
2021-07-26 16:28:07 +0900

    add: Day05

--- 2021/Day05.php
+++ 2021/Day05.php
@@ -11,4 +11,20 @@
 $input_array = str_replace(array('[', ']', ','), '', trim(fgets(STDIN)));
 $array = explode(' ', $input_array);
 
-var_dump($array);
+var_dump(BubbleSort($array));
+
+function BubbleSort($array)
+{
+    for ($i=0; $i<count($array); $i++) {
+        $newLength = count($array) - $i;
+        for ($j=1; $j<$newLength; $j++) {
+            if ($array[$j-1] > $array[$j]) {
+                $tmp = $array[$j];
+                $array[$j] = $array[$j-1];
+                $array[$j-1] = $tmp;
+            }
+        }
+    }
+
+    return $array;
+}
