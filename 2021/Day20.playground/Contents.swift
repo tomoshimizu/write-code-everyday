a757edb5ec4dc5f327b32e31bccbec889839ad9d
tomoshimizu <tomotomo.061160@gmail.com>
2021-12-31 14:47:24 +0900

    add: Day20 and Day21

---
+++ 2021/Day20.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// Section3: Arrays
+
+let ints = [Int]()
+let strings = [String]()
+//let pepple = [Person]()
+
+var array = ["a", "b", "c", "d"]
+//array.insert("e", at: 99)
+array.append("e")
+array.insert("b", at: 1)
+array.remove(at: 1)
+print(array)
